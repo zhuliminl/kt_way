dedb0dc04458c85a8299452e46ffb79576fb5e37
zhuliminl <zhuliminl@gmail.com>
2021-07-25 22:19:39 +0800

    完成底部导航，难点是 navigationView 在 kotlin 下的引入问题。需要 extension

--- app/src/main/java/com/example/kt_way/MainActivity.kt
+++ app/src/main/java/com/example/kt_way/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.kt_way
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+
+//import kotlinx.android.
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration : AppBarConfiguration = AppBarConfiguration(setOf(
+            R.id.recipesFragment,
+            R.id.favoriteRecipesFragment,
+            R.id.foodJokesFragment ))
+
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+//
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() ||  super.onSupportNavigateUp()
     }
 }
