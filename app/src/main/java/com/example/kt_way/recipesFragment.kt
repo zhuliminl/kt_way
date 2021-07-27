ac7b9fac7f69548cdebcd7e8e844e8f4b0246f16
zhuliminl <zhuliminl@gmail.com>
2021-07-27 23:27:17 +0800

    实现 骨架图的展示

--- app/src/main/java/com/example/kt_way/recipesFragment.kt
+++ app/src/main/java/com/example/kt_way/recipesFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
 class recipesFragment : Fragment() {
 
@@ -16,7 +17,9 @@ class recipesFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        val view = inflater.inflate(R.layout.fragment_recipes, container, false)
+        view.shimmer_recycler_view.showShimmer();
+        return view
     }
 
 }
