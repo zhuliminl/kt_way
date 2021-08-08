b65b096f65b0bda2616e59343f176fc90f2fec02
zhuliminl <zhuliminl@gmail.com>
2021-08-08 22:00:08 +0800

    初始化 ViewModel

---
+++ app/src/main/java/com/example/kt_way/viewModels/MainViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.kt_way.viewModels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainViewModel(application: Application): AndroidViewModel(application) {
+    
+}
