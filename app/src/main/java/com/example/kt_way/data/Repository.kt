a4c1b76da25364fb91fc41b69422a554ea392d14
zhuliminl <zhuliminl@gmail.com>
2021-08-09 01:55:23 +0800

    创建 ViewModel

--- app/src/main/java/com/example/kt_way/data/Repository.kt
+++ app/src/main/java/com/example/kt_way/data/Repository.kt
@@ -1,7 +1,9 @@
 package com.example.kt_way.data
 
+import dagger.hilt.android.scopes.ActivityRetainedScoped
 import javax.inject.Inject
 
+@ActivityRetainedScoped
 class Repository @Inject constructor(
     remoteDataSource: RemoteDataSource
 ){
--- app/src/main/java/com/example/kt_way/viewModels/MainViewModel.kt
+++ app/src/main/java/com/example/kt_way/viewModels/MainViewModel.kt
@@ -1,8 +1,75 @@
 package com.example.kt_way.viewModels
 
 import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import androidx.lifecycle.AndroidViewModel
+import com.example.kt_way.data.Repository
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MutableLiveData
+import com.example.kt_way.models.FoodRecipe
+import com.example.kt_way.utils.NetworkResult
+import retrofit2.Response
 
-class MainViewModel(application: Application): AndroidViewModel(application) {
-    
+
+class MainViewModel @ViewModelInject constructor(
+    private val repository: Repository, application: Application
+) : AndroidViewModel(application) {
+
+    var recipesResponse: MutableLiveData<NetworkResult<FoodRecipe>> = MutableLiveData()
+
+    // 获取 recipes response 的值
+    private suspend fun getRecipesSafeCall(queries: Map<String, String>) {
+        recipesResponse.value = NetworkResult.Loading()
+        if (hasInternetConnection()) {
+            try {
+                val response = repository.remote.getRecipes(queries)
+                recipesResponse.value = handleFoodRecipesResponse(response)
+            } catch (e: Exception) {
+                recipesResponse.value = NetworkResult.Error("not found")
+            }
+        } else {
+            recipesResponse.value = NetworkResult.Error("没有网络")
+        }
+    }
+
+    // 处理 response 的各种结果，包装成 NetworkResult 的各种异常结果返回
+    private fun handleFoodRecipesResponse(response: Response<FoodRecipe>): NetworkResult<FoodRecipe> {
+        when {
+            response.message().toString().contains("timeout") -> {
+                return NetworkResult.Error("请求超时")
+            }
+            response.code() == 402 -> {
+                return NetworkResult.Error("API key limited")
+            }
+            response.body()!!.results.isNullOrEmpty() -> {
+                return NetworkResult.Error("Recipes not found")
+            }
+            response.isSuccessful -> {
+                val foodRecipes = response.body()
+                return NetworkResult.Success(foodRecipes!!)
+            }
+            else -> {
+                return NetworkResult.Error(response.message())
+            }
+
+        }
+    }
+
+
+    // 检查网络
+    private fun hasInternetConnection(): Boolean {
+        val connectivityManager =
+            getApplication<Application>().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetwork ?: return false
+        val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork) ?: return false
+        return when {
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> return false
+        }
+    }
+
 }
